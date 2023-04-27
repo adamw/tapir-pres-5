76cf948a836f079080a8dcf6331cb08e4c341c2a
adamw <adam@warski.org>
2023-04-27 11:06:37 +0200

    WIP

--- src/main/scala/demo/S160_Tapir_Demo.scala
+++ src/main/scala/demo/S160_Tapir_Demo.scala
@@ -1,117 +1,86 @@
 package demo
 
-object S160_Tapir_Demo extends App {
-  case class Book(title: String, year: Int)
+import io.circe.generic.auto.*
+import sttp.tapir.*
+import sttp.tapir.generic.auto.*
+import sttp.tapir.json.circe.*
+import sttp.tapir.server.interceptor.{Interceptor, RequestInterceptor}
+import sttp.tapir.server.netty.loom.{Id, NettyIdServer, NettyIdServerOptions}
+import sttp.tapir.swagger.SwaggerUI
+import sttp.tapir.swagger.bundle.SwaggerInterpreter
 
-  var books = List(
-    Book("The Sorrows of Young Werther", 1774),
-    Book("Iliad", -8000),
-    Book("Nad Niemnem", 1888),
-    Book("The Colour of Magic", 1983),
-    Book("The Art of Computer Programming", 1968),
-    Book("Pharaoh", 1897),
-    Book("Lords and Ladies", 1992)
-  )
+// data structures
 
-  object Endpoints {
-    import io.circe.generic.auto._
-    import sttp.tapir._
-    import sttp.tapir.generic.auto._
-    import sttp.tapir.json.circe._
+case class Book(title: String, year: Int)
 
-    val baseEndpoint = endpoint.in("books").errorOut(stringBody)
-    val limitParameter = query[Option[Int]]("limit").description("Limit parameter")
+var books = List(
+  Book("The Sorrows of Young Werther", 1774),
+  Book("Iliad", -8000),
+  Book("Nad Niemnem", 1888),
+  Book("The Colour of Magic", 1983),
+  Book("The Art of Computer Programming", 1968),
+  Book("Pharaoh", 1897),
+  Book("Lords and Ladies", 1992)
+)
 
-    // POST /books/add
-    val addBook: Endpoint[(Book, String), String, Unit, Any] = baseEndpoint.post
-      .in("add")
-      .in(jsonBody[Book].example(Book("Pride and prejudice", 1813)))
-      .in(header[String]("X-Auth-Token").description("The token is 'secret'"))
+// endpoints
 
-    // GET /books?year=...&limit=...
-    val listBooks: Endpoint[(Option[Int], Option[Int]), String, List[Book], Any] =
-      baseEndpoint.get
-        .in(query[Option[Int]]("year"))
-        .in(limitParameter)
-        .out(jsonBody[List[Book]])
-  }
+val baseEndpoint = endpoint.in("books").errorOut(stringBody)
+val limitParameter = query[Option[Int]]("limit").description("Limit parameter")
 
-  //
+// POST /books/add
+val addBook: PublicEndpoint[(Book, String), String, Unit, Any] = baseEndpoint.post
+  .in("add")
+  .in(jsonBody[Book].example(Book("Pride and prejudice", 1813)))
+  .in(header[String]("X-Auth-Token").description("The token is 'secret'"))
 
-  import Endpoints._
-  import akka.http.scaladsl.server.Route
+// GET /books?year=...&limit=...
+val listBooks: PublicEndpoint[(Option[Int], Option[Int]), String, List[Book], Any] =
+  baseEndpoint.get
+    .in(query[Option[Int]]("year"))
+    .in(limitParameter)
+    .out(jsonBody[List[Book]])
 
-  def booksRoutes: Route = {
-    import sttp.tapir.server.akkahttp._
+// server logic
 
-    import scala.concurrent.ExecutionContext.Implicits.global
-    import scala.concurrent.Future
-
-    val addBookServerEndpoint = addBook.serverLogic { case (book, token) =>
-      Future {
-        if (token != "secure") {
-          Left("Unauthorized access!!!11")
-        } else {
-          books = book :: books
-          Right(())
-        }
-      }
-    }
-
-    val listBooksServerEndpoint = listBooks.serverLogic[Future] { case (year, limit) =>
-      Future {
-        val filteredBooks = year match {
-          case Some(y) => books.filter(_.year == y)
-          case None    => books
-        }
-        val limitedBooks = limit match {
-          case Some(l) => filteredBooks.take(l)
-          case None    => filteredBooks
-        }
-        Right(limitedBooks)
-      }
-    }
-
-    AkkaHttpServerInterpreter.toRoute(List(addBookServerEndpoint, listBooksServerEndpoint))
-  }
-
-  def openapiYamlDocumentation: String = {
-    import sttp.tapir.docs.openapi._
-    import sttp.tapir.openapi.circe.yaml._
-
-    val docs: OpenAPI = OpenAPIDocsInterpreter.toOpenAPI(List(addBook, listBooks), "Books I've read", "1.0")
-    docs.toYaml
-  }
-
-  def startServer(): Unit = {
-    import akka.actor.ActorSystem
-    import akka.http.scaladsl.Http
-    import akka.http.scaladsl.server.Directives._
-    import sttp.tapir.swagger.akkahttp.SwaggerAkka
-
-    import scala.concurrent.Await
-    import scala.concurrent.duration._
-
-    val routes = booksRoutes ~ new SwaggerAkka(openapiYamlDocumentation).routes
-    implicit val actorSystem: ActorSystem = ActorSystem()
-    Await.result(Http().newServerAt("localhost", 8080).bind(routes), 1.minute)
-  }
-
-  def makeClientRequest(): Unit = {
-    import sttp.client3._
-    import sttp.tapir.client.sttp._
-
-    val backend: SttpBackend[Identity, Any] = HttpURLConnectionBackend()
-
-    val booksListingRequest: Request[Either[String, List[Book]], Any] = SttpClientInterpreter
-      .toRequestThrowDecodeFailures(listBooks, Some(uri"https://souoxj2dr5.execute-api.eu-central-1.amazonaws.com"))
-      .apply((None, Some(2)))
-
-    val result: Either[String, List[Book]] = booksListingRequest.send(backend).body
-    println("Client call result: " + result)
-  }
-
-//  startServer()
-  makeClientRequest()
-//  println("Try out the API by opening the Swagger UI: http://localhost:8080/docs")
+val addBookServerEndpoint = addBook.serverLogic[Id] { case (book, token) =>
+  if token != "secure" then Left("Unauthorized access!!!11")
+  else
+    books = book :: books
+    Right(())
 }
+
+val listBooksServerEndpoint = listBooks.serverLogic[Id] { case (year, limit) =>
+  val filteredBooks = year match {
+    case Some(y) => books.filter(_.year == y)
+    case None    => books
+  }
+  val limitedBooks = limit match {
+    case Some(l) => filteredBooks.take(l)
+    case None    => filteredBooks
+  }
+  Right(limitedBooks)
+}
+
+def startServer(): Unit =
+  val mainEndpoints = List(addBookServerEndpoint, listBooksServerEndpoint)
+  val docEndpoints = SwaggerInterpreter().fromServerEndpoints(mainEndpoints, "Books API", "1.0")
+
+  NettyIdServer().addEndpoints(mainEndpoints ++ docEndpoints).start()
+
+def startServerWithInterceptor(): Unit =
+  val mainEndpoints = List(addBookServerEndpoint, listBooksServerEndpoint)
+  val docEndpoints = SwaggerInterpreter().fromServerEndpoints(mainEndpoints, "Books API", "1.0")
+
+  val customOptions = NettyIdServerOptions.customiseInterceptors
+    .prependInterceptor(RequestInterceptor.transformServerRequest { request =>
+      println("Handling request: " + request)
+      request
+    })
+    .options
+
+  NettyIdServer(customOptions).addEndpoints(mainEndpoints ++ docEndpoints).start()
+
+@main def start(): Unit =
+  startServerWithInterceptor()
+  println("Try out the API by opening the Swagger UI: http://localhost:8080/docs")
