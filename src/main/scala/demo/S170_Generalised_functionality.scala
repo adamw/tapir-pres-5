3026e042a6d55e0a9d7c0d5f7886e469a7dcfbba
adamw <adam@warski.org>
2023-05-23 12:32:53 +0200

    WIP

---
+++ src/main/scala/demo/S170_Generalised_functionality.scala
@@ -0,0 +1,18 @@
+package demo
+
+object S170_Generalised_functionality:
+  // interceptors
+  val metrics = true
+  val logging = true
+  val exceptionHandling = true
+  val contentNegotiation = true
+  val CORS = true
+
+  // modules
+  val staticFiles = true
+
+  // data formats
+  val JSON = true
+  val XML = true
+  val Enumerations = true
+  val InheritanceHierarchies = true
